2c93ed39100570644793503e84ebd7d64ee1dd4f
Sarah Arnold <digipet007@gmail.com>
2019-10-30 14:15:50 -0700

    added JAWSDB_URL for Heroku connection

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,17 @@
 var mysql = require('mysql');
+var connection;
 
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'flowerfa1!',
-    database: 'burgers_db'
-})
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'flowerfa1!',
+        database: 'burgers_db'
+    })
+}
 
-connection.connect(function(err){
-    if(err) throw err;
-    console.log("connected as id: " + connection.threadId);
-})
 
+connection.connect();
 module.exports = connection;
