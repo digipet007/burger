95d0c52af13e6b5911fb1c07d6a9123be802662f
Sarah Arnold <digipet007@gmail.com>
2019-11-17 23:37:43 -0800

    updated validate function and changed form value

--- public/assets/javascript/validate.js
+++ public/assets/javascript/validate.js
@@ -1,11 +1,7 @@
 $(document).ready(function() {
-  console.log("document loaded");
-  let burgerRequest = $("#burger_request")
-    .val()
-    .trim();
+  var burgerRequest = $("#burger_request").val();
   $("#submit-button").on("click", function() {
     if (!burgerRequest) {
-      console.log("no burger request");
       alert("please enter a burger name");
       return false;
     }
